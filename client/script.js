2bd47594d3087e3f813e7fc2ccffb31ded6412b0
yosrimhamdi <yosrimhamdi@outlook.com>
2021-07-17 19:45:23 +0100

    removed dist and .cache client folder.

--- client/script.js
+++ client/script.js
@@ -2,4 +2,8 @@ import { io } from 'socket.io-client';
 
 const socket = io('http://localhost:3000');
 
+socket.on('connect', () => {
+  console.log(socket.id);
+});
+
 console.log('this is great!');
