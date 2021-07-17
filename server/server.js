2bea0e3aa92f3082d9feee46bc0eb07dbe34c98e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-07-17 23:36:24 +0100

    updated some code.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
-const io = require('socket.io')(3000, {
-  cors: {
-    origin: '*',
-  },
-});
+const io = require('socket.io')(3000, { cors: { origin: '*' } });
 
 io.on('connection', socket => {
   socket.on('message', message => {
-    io.emit('message', socket.id, message);
+    socket.broadcast.emit('message', socket.id, message);
   });
 });
