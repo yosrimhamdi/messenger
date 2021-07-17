ad5d1bc1459b58ed2b69b742f78de000c032ecd0
yosrimhamdi <yosrimhamdi@outlook.com>
2021-07-17 18:24:15 +0100

    fixing cors issue.

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-console.log(io);
+const socket = io('http://localhost:3000');
 
-console.log('hi');
+console.log('this is great!');
--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
-const io = require('socket.io')();
+const io = require('socket.io')(3000, {
+  cors: {
+    origin: ['http://localhost:1234'],
+  },
+});
 
 io.on('connection', socket => {
   console.log(socket.id);
